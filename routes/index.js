a0f075d04438ed09430bc822fbe85525595337a9
Equan Pr <byte@equan.me>
2015-06-25 18:15:35 +0700

    basic route

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+     
+    ('OPTIONS' == req.method) ? res.send(200) : next();
+    
+});
+
+
+router.get('/customer', function(req, res, next){
+    res.json({
+        customer: 'just me'
+    })
+})
