dfd0418fd1b7ce4c8260809ccbb1f14b71b5c88c
Equan Pr <byte@equan.me>
2015-06-25 19:09:30 +0700

    simple customer model

---
+++ models/customer.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var MONGODB_URI = process.env.MONGOLAB_URI || 'mongodb://localhost/barbershop';
+var db = mongoose.createConnection(MONGODB_URI);
+
+db.on('connected', function () {
+    console.log('Connected to MongoDB.');
+});
+
+db.on('error', function (err) {
+    console.error.bind(console, 'Connection to MongoDB error!.');
+});
+
+// Customer Schema
+var CustomerSchema = new mongoose.Schema({
+        name: 'string',
+        email: 'string',
+        createdAt: 'date',
+        updatedAt: 'date'
+    });
+
+module.exports = db.model('Customer', CustomerSchema);
